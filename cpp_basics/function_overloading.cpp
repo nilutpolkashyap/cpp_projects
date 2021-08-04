203610d8f19439b57ca2f7253f7ffbc07ce4f991
Nilutpol Kashyap <nkelectronicshlnp@gmail.com>
2021-08-04 23:45:50 +0530

    commit

--- cpp_basics/function_overloading.cpp
+++ cpp_basics/function_overloading.cpp
@@ -1,17 +1,20 @@
 #include <iostream>
 using namespace std;
 
-int sum(int w, int x, int y, int z);
-int sum(int w, int x, float y, float z);
+int sum(int , int , int , int);
+float sum(float , float , float , float );
+float sum(float, float, float);
 
 int main()
 {
     int a, b, c, d;
 
-    cout << "\nsum(3,8) : " << sum(3,8,4);
-    cout << "\nsum(3,8,2,4) : " << sum(3,8,2,4);
+    cout << "\nsum(4,3,9,5) : " << sum(4,3,9,5);
+    //cout << "\nsum(4,9) : " << sum(4,9);
+    //cout << "\nsum(9.1, 4.5, 6.3) : " << sum(9.1, 4.5,6.3);
+    cout << "\nsum(6.0,9.7,3.2) : " << sum(6 ,9 , 3.2, 6.5);
 
-    cout << "\nsum(6,9,3.2) : " << sum(6,9, 3.2, 6.5);
+    cout << "\nsum(6.0,9.7,3.2) : " << sum(6.2 , 3.2, 6.5);
 
 }
 
@@ -20,7 +23,13 @@ int sum(int w, int x, int y = 5, int z = 2)
     return (w +x+y+z);
 }
 
-int sum(int w, int x, float y = 3.4, float z= 7.3)
+float sum(float w, float x, float y, float z)
 {
     return (w+x+y+z);
 }
+
+float sum(float a, float b, float c)
+{
+    return (a + b + c);
+}
+
