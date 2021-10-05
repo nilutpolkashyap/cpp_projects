48d8c939e236d00ea0f4d8ee5eb3da8870617b9c
Priyanka K <54153547+thepriyankakashyap@users.noreply.github.com>
2021-10-06 00:12:27 +0530

    uploaded my files

---
+++ cpp_basics/priyanka_basics/hello_world.cpp
@@ -0,0 +1,391 @@
+#include <iostream>
+#include <string> //header for string class
+using namespace std;
+
+int main()
+
+{
+    /*
+    //variable declaration
+    int a;
+
+    //variable initialization
+    a=1;
+
+    cout <<"a=" <<a <<endl;
+
+    //c-line initialization
+    int b=2;
+
+     cout <<"b=" <<b <<endl;
+    
+    //uniform initialization
+    int r {8};
+
+    cout <<"today is calm=" <<r <<endl;
+
+    //character variabble
+    char h {'a'};
+
+    cout <<"h=" <<h <<endl;
+
+    cout << h << endl; 
+
+    //float variable
+    float w {2.5};
+
+    cout <<"w=" <<w <<endl; 
+    w = 9.8;
+    cout <<"w=" <<w <<endl;
+
+    //boolean variable
+    bool q {1};
+
+    cout <<"q=" <<q <<endl;
+    q=false;
+
+    cout <<"q=" <<q <<endl;
+
+    //string type variable
+    string i = "kutta";
+
+    cout <<"i=" <<i <<endl;
+
+    i = "kutti";
+    
+    cout <<"i=" <<i <<endl;
+
+
+    int y = 9, z = 15, result;
+
+    cout <<"y=" <<y <<endl;
+    cout <<"z=" <<z <<endl;
+
+    //arithmetic operator
+    result= y+z;
+
+    //= is known as assignment operator 
+    //+ is addition operator 
+    cout <<"9+15=" <<result <<endl;
+
+    result = y-z;
+
+    //- is subtraction operator
+    cout <<"9-15=" <<result <<endl;
+
+    result = y*z;
+
+    //* is multipication operator
+    cout <<"9*15=" <<result <<endl;
+
+    result = y/z;
+
+    // / is division operator
+    cout <<"9/15=" <<result <<endl;
+    */
+
+    // if statement
+
+    /*
+    int num;
+
+    cout <<"enter a number" <<endl;
+    
+    // to get user input
+    cin>> num;
+
+    cout <<"num=" <<num <<endl;
+
+    int mod;
+
+    // % is the modulus symbol
+    mod = num % 2;
+    */
+
+    // == is for comparison
+    // == compares two values or equality (true or false)
+
+    /*
+    if ( mod == 1)
+
+    {
+
+        cout << "odd";
+        
+    }
+        // else is for 'or'
+        else 
+    {
+
+        cout << "even";
+
+    }
+    */
+    /*
+    // != not equal to operator
+     if ( mod != 1 )
+
+     {
+         cout <<"even" <<endl;
+
+     }
+     else 
+
+     {
+
+         cout <<"odd" <<endl;
+
+     }
+    */
+
+
+    /*
+     char sneha;
+
+     cout <<"enter a character" <<endl;
+
+     cin>> sneha;
+
+     cout <<"sneha=" <<sneha <<endl;
+
+    if (sneha == 'h')
+
+    {
+        cout <<"hemanga" <<endl;
+
+    }
+
+    else if (sneha == 'l')
+
+    {
+        cout <<"labanya" <<endl;
+
+    }
+
+    else if (sneha == 'n')
+
+    {
+        cout <<"nil" <<endl;
+
+    }
+
+    else if (sneha == 'p')
+
+    {
+        cout <<"priyanka" <<endl;
+    }
+    
+    else 
+    
+    {
+        cout <<"error" <<endl;
+    }
+    */
+
+   /*
+    for ( int i = 8 ; i <= 10 ; ++i )
+
+    {
+        cout << i << endl;
+    }
+
+
+    for ( int u = 2 ; u < 9 ; ++u )
+
+    {
+        cout << u << endl;
+    }
+    */
+   /*
+    for (int t = 14 ; t >= 4 ; --t)
+
+    {
+        cout <<t <<endl;
+
+    }
+
+    for (int t = 14 ; t >= 4 ; ++t)
+
+    { 
+        cout <<t << endl;
+    }
+    */
+
+   /*
+   for (int x = 1 ; x <= 20 ; ++x)
+
+    {
+        cout <<x <<endl;
+    }
+    */
+    
+    /*
+    for ( int j = 4 ; j >= 1 ; --j)
+
+    {
+
+        cout <<j <<endl;
+
+    } 
+
+    for ( int  k = 2 ; k <= 100 ; k = k + 1)
+
+    {
+
+        cout <<k <<endl;
+
+    }
+    */
+   /*
+    for ( int f = 1000 ; f >= 6 ; f = f- 1)
+
+    {
+
+        cout <<f <<endl;
+
+    }
+    */
+   /*
+   for ( int o = 3 ; o <= 60 ; o = o + 3)
+
+   {
+
+       cout <<o <<endl;
+
+   }
+    */
+   /*
+   for ( int u = 0 ; u <= 10 ; ++u )
+
+   {
+
+       cout <<u <<endl;
+
+   }
+    */
+   /*
+   for ( int  t = 9 ; t <= 90 ; ++t )
+   
+    {
+
+        cout <<t <<endl;
+
+        int rem = t % 2 ;
+
+        if ( rem == 0 )
+
+        {
+
+            cout <<"even" <<endl ;
+
+        }
+
+        else 
+
+        {
+
+            cout << "odd" <<endl;
+
+        }
+    
+    }
+    */
+   /*
+    int q = 9;
+
+    while ( q < 99 )
+
+    { 
+
+        cout <<q <<endl;
+
+        ++q;
+
+    }
+    */
+   /*
+
+    int p = 50;
+
+    while ( p > 0 )
+
+    { 
+
+        cout << p << endl;
+
+        --p;
+    }
+    */
+   /*
+    int l = 100000;
+
+    while ( l > 45 )
+
+    {
+
+        cout <<l <<endl;
+
+        --l;
+    }
+
+    */
+
+   /*
+    int h = 0;
+
+    while ( h < 50 )
+
+    { 
+
+        cout <<h <<endl;
+
+        h = h + 3;
+
+    }
+    */
+
+
+    int e ;
+
+    cout <<"enter a number" <<endl;
+
+    cin >>e;
+
+    cout <<"e=" <<e <<endl;
+
+    /*
+    for ( int n = 0 ; n <= e ; ++n )
+
+    {
+
+        cout <<n <<endl;
+
+    }
+    */
+
+    int f = 0 ;
+
+    while ( f < e )
+
+    {
+
+        cout<<f <<endl;
+
+        ++f;
+    }
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+
+} 
