5f4e5edbe0fc540030c2e7ac9f6f0de728537b33
Nilutpol Kashyap <nkelectronicshlnp@gmail.com>
2022-03-10 23:31:42 +0530

    Update friend_function.cpp

--- oops/friend_function.cpp
+++ oops/friend_function.cpp
@@ -5,7 +5,6 @@ class Distance {
     private:
         int meter;
         
-        // friend function
         friend int addFive(Distance);
 
     public:
@@ -13,10 +12,8 @@ class Distance {
         
 };
 
-// friend function definition
 int addFive(Distance d) {
 
-    //accessing private members from the friend function
     d.meter += 5;
     return d.meter;
 }
