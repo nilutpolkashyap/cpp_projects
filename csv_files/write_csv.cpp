545456a8842b019655309a3d7813e69af6bfee01
Nilutpol Kashyap <nkelectronicshlnp@gmail.com>
2022-06-01 23:44:28 +0530

    writing csv files in c++

---
+++ csv_files/write_csv.cpp
@@ -0,0 +1,15 @@
+#include <fstream>
+#include <iostream>
+
+int main(int argc, char* argv[])
+{
+    std::ofstream myfile;
+    myfile.open ("example.csv");
+    myfile << "This is the first cell in the first column.\n";
+    myfile << "a,b,c,\n";
+    myfile << "c,s,v,\n";
+    myfile << "1,2,3.456\n";
+    myfile << "semi;colon";
+    myfile.close();
+    return 0;
+}
